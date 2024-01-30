eae28f1ad0b233c27a78688d1a0cdd936a23ec63
dominic <dominicf2001@gmail.com>
2024-01-29 23:12:39 -0500

    Refactor to fetch and display hello world response from get request to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
+    constructor(private http: HttpClient) { }
+
+    response: string = ''
+    
+    getRequest() {
+        this.http.get('http://localhost:8080/', {responseType: "text"}).subscribe({
+            next: res => {
+                this.response = res
+            },
+            error: err => {
+                console.error(err);
+            }
+        });
+    }
+    
     title = 'temp-name';
 }
