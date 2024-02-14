c111e97b82982b4dec04f3a89b646cd0a829f596
Dominic <dominicf2001@gmail.com>
2024-02-14 06:51:49 -0500

    Define colors and style sidebar with img

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.html.ts",
+        "./src/**/*.{html,ts}",
     ],
     theme: {
+        colors: {
+            white: "#EFECEC",
+            blue: "#182332"
+        },
         extend: {},
     },
     plugins: [],
 }
-
