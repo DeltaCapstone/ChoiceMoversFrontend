7ef132fb5c80beceae6bd8b43eea52e0afccd716
dominic <dominicf2001@gmail.com>
2024-02-15 16:50:54 -0500

    add components for each sidebar view

---
+++ src/components/schedule/schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './schedule.component.html',
+  styleUrl: './schedule.component.css'
+})
+export class ScheduleComponent {
+
+}
