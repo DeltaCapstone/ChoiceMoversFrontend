0ad0feca50109d0e9a8e144a878961aa41302cd3
Dominic <dominicf2001@gmail.com>
2024-02-15 14:45:32 -0500

    Set up basic fullcalendar

--- src/components/schedule/schedule.component.ts
+++ src/components/schedule/schedule.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [],
+    imports: [FullCalendarModule],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
 })
 export class ScheduleComponent {
-
+    calendarOptions: CalendarOptions = {
+        initialView: 'dayGridMonth',
+        plugins: [dayGridPlugin]
+    }
 }
