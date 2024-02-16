b3938ee563315f3287274aaa812ec689d7aab00b
Dominic <dominicf2001@gmail.com>
2024-02-16 13:49:37 -0500

    scaffold table

--- src/components/employees/employees.component.ts
+++ src/components/employees/employees.component.ts
@@ -1,12 +1,63 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {TuiTableModule} from '@taiga-ui/addon-table';
+import { TuiLetModule } from '@taiga-ui/cdk';
+import { TuiTagModule } from '@taiga-ui/kit';
+import {TuiButtonModule} from '@taiga-ui/core';
+
+interface User {
+    readonly email: string;
+    readonly name: string;
+    readonly status: 'alive' | 'deceased';
+    readonly tags: readonly string[];
+}
 
 @Component({
-  selector: 'app-employees',
-  standalone: true,
-  imports: [],
-  templateUrl: './employees.component.html',
-  styleUrl: './employees.component.css'
+    selector: 'app-employees',
+    standalone: true,
+    imports: [TuiTableModule, TuiTagModule, NgIf, NgFor, TuiLetModule, TuiButtonModule],
+    templateUrl: './employees.component.html',
+    styleUrl: './employees.component.css'
 })
 export class EmployeesComponent {
+    readonly columns = ['name', 'email', 'status', 'tags'];
 
+    users: readonly User[] = [
+        {
+            name: 'Michael Palin',
+            email: 'm.palin@montypython.com',
+            status: 'alive',
+            tags: ['Funny'],
+        },
+        {
+            name: 'Eric Idle',
+            email: 'e.idle@montypython.com',
+            status: 'alive',
+            tags: ['Funny', 'Music'],
+        },
+        {
+            name: 'John Cleese',
+            email: 'j.cleese@montypython.com',
+            status: 'alive',
+            tags: ['Funny', 'Tall', 'Actor'],
+        },
+        {
+            name: 'Terry Jones',
+            email: '',
+            status: 'deceased',
+            tags: ['Funny', 'Director'],
+        },
+        {
+            name: 'Terry Gilliam',
+            email: 't.gilliam@montypython.com',
+            status: 'alive',
+            tags: ['Funny', 'Director'],
+        },
+        {
+            name: 'Graham Chapman',
+            email: '',
+            status: 'deceased',
+            tags: ['Funny', 'King Arthur'],
+        },
+    ];
 }
