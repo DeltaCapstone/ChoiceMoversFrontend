5f1eaf3dfe12c4ccd69677e89ec4fac622640455
Josh Romisher <jromisher28@gmail.com>
2024-02-18 14:49:15 -0500

    initial commit of home, needs work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ScheduleComponent } from '../components/schedule/schedule.component';
 import { EmployeesComponent } from '../components/employees/employees.component';
 import { StatisticsComponent } from '../components/statistics/statistics.component';
 import { SettingsComponent } from '../components/settings/settings.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'schedule', pathMatch: 'full'},
-    {path: 'schedule', component: ScheduleComponent},
-    {path: 'employees', component: EmployeesComponent},
-    {path: 'statistics', component: StatisticsComponent},
-    {path: 'settings', component: SettingsComponent}
+    { path: '', redirectTo: 'schedule', pathMatch: 'full' },
+    { path: 'schedule', component: ScheduleComponent },
+    { path: 'employees', component: EmployeesComponent },
+    { path: 'statistics', component: StatisticsComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: 'home', component: HomeComponent }
 ];
