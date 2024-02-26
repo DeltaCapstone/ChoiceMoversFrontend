73d8ed0a2a5e2589fbe30f9f8eb2219998fcaea5
Josh Romisher <jromisher28@gmail.com>
2024-02-25 20:14:45 -0500

    move-planner-blowout init and move-planner button edit

---
+++ src/app/content/home/move-planner-blowout/move-planner-blowout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-move-planner-blowout',
+  standalone: true,
+  imports: [],
+  templateUrl: './move-planner-blowout.component.html',
+  styleUrl: './move-planner-blowout.component.css'
+})
+export class MovePlannerBlowoutComponent {
+
+}
