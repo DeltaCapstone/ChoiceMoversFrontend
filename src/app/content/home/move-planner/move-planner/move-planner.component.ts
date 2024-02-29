9f61842483749040525338b1a615baa45a134e29
Josh Romisher <jromisher28@gmail.com>
2024-02-29 18:55:14 -0500

    move planner drop down and title

--- src/app/content/home/move-planner/move-planner/move-planner.component.ts
+++ src/app/content/home/move-planner/move-planner/move-planner.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgFor } from '@angular/common';
+import { MoveBlowoutDropdownComponent } from '../../../../shared/components/move-blowout-dropdown/move-blowout-dropdown.component';
+
 @Component({
   selector: 'app-move-planner',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, MoveBlowoutDropdownComponent],
   templateUrl: './move-planner.component.html',
   styleUrl: './move-planner.component.css'
 })
--- src/app/shared/components/move-blowout-dropdown/move-blowout-dropdown.component.ts
+++ src/app/shared/components/move-blowout-dropdown/move-blowout-dropdown.component.ts
@@ -1,29 +1,53 @@
 import { Component } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { TuiButtonModule, TuiDropdownModule } from '@taiga-ui/core';
+import { NgFor, } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
+
+type DropDownItem = {
+  readonly room: string
+  readonly route: string
+}
 @Component({
   selector: 'app-move-blowout-dropdown',
   standalone: true,
-  imports: [TuiButtonModule, TuiDropdownModule],
+  imports: [TuiButtonModule, TuiDropdownModule, NgFor, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './move-blowout-dropdown.component.html',
   styleUrl: './move-blowout-dropdown.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MoveBlowoutDropdownComponent {
-  open = false;
+  
+  items: DropDownItem[] = [
+    {room: 'Bedroom', route: 'bedroom'},
+    {room: 'Bathroom', route: 'bathroom'},
+    {room: 'Garage', route: 'garage'},
+    {room: 'Utility Room', route: 'utility-room'},
+    {room: 'Patio/Deck', route: 'patio'},
+    {room: 'Study', route: 'study'},
+    {room: 'Kitchen', route: 'kitchen'},
+    {room: 'Living Room', route: 'living-room'}     
+  ];
+
+  selectedOption: string;
+
+  open: boolean = false;
 
   onClick(): void {
     this.open = !this.open;
   }
 
-  onObscured(obscured: boolean) {
+  onObscured(obscured: boolean): void {
     if (obscured) {
       this.open = false;
     }
   }
 
-  onActiveZone(active: boolean) {
-    this.open = active && this.open;
+  onActiveZone(active: boolean): void {
+    if(active) {
+      this.open = true;
+    }
   }
 }
