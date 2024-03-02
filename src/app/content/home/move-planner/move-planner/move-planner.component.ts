9bd116bd51c1c6421b61b5e32843b1d2c5e30c6c
Josh Romisher <jromisher28@gmail.com>
2024-03-01 20:29:13 -0500

    progress on move planner modal

--- src/app/content/home/move-planner/move-planner/move-planner.component.ts
+++ src/app/content/home/move-planner/move-planner/move-planner.component.ts
@@ -11,7 +11,8 @@ import { MatIconModule } from '@angular/material/icon'
 type DropDownItem = {
   readonly room: string;
   readonly route: string;
-  readonly roomItems: string[];
+  readonly roomItems: { itemName: string, icon: string }[];
+  readonly icon: string;
 }
 
 @Component({
@@ -28,54 +29,71 @@ export class MovePlannerComponent {
 
   items: DropDownItem[] = [
     {
-      room: 'Bedroom', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Bedroom', route: 'move-planner-modal', icon: 'bed', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'chair' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'chair' }, { itemName: 'Lighting', icon: 'chair' }
       ]
     },
     {
-      room: 'Bathroom', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Bathroom', route: 'move-planner-modal', icon: 'soap', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Garage', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Garage', route: 'move-planner-modal', icon: 'garage', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Utility Room', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Utility Room', route: 'move-planner-modal', icon: 'local_laundry_service', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Patio/Deck', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Patio/Deck', route: 'move-planner-modal', icon: 'deck', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Study', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Study', route: 'move-planner-modal', icon: 'book', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Kitchen', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Kitchen', route: 'move-planner-modal', icon: 'kitchen', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     },
     {
-      room: 'Living Room', route: 'move-planner-modal', roomItems: [
-        'Bed Frame', 'Mattress', 'Paintings', 'Chairs', 'T.V.', 'Nightstand', 'Dresser', 'Lighting'
+      room: 'Living Room', route: 'move-planner-modal', icon: 'chair', roomItems: [
+        { itemName: 'Bed Frame', icon: 'bed' }, { itemName: 'Mattress', icon: 'single_bed' }, { itemName: 'Paintings', icon: 'wall_art' }, { itemName: 'Chairs', icon: 'chair' }, { itemName: 'T.V.', icon: 'tv' }, { itemName: 'Nightstand', icon: 'table_restaurant' }, { itemName: 'Dresser', icon: 'dresser' }, { itemName: 'Lighting', icon: 'floor_lamp' }
       ]
     }
   ]
 
   constructor(public dialog: MatDialog) { }
 
-  openRoomModal(room: string, roomItems: string[]): void {
+  getIconName(itemName: string): string {
+    const item = this.items.find(item => item.room === itemName);
+    return item ? item.icon : 'house';
+  }
+
+  getRoomItem(roomItems: { itemName: string, icon: string }[]): string[] {
+    return roomItems.map(item => item.itemName);
+  }
+
+  getListIcon(roomItems: { itemName: string, icon: string }): string {
+    return roomItems.icon;
+  }
+
+  openRoomModal(room: string, roomItems: { itemName: string, icon: string }[]): void {
+    //Getting the icon for the room title
+    const iconName = this.getIconName(room);
+    //Getting icons for the specific room items
+    const iconsList = roomItems.map(item => ({ itemName: item.itemName, icon: this.getListIcon(item) }));
     const dialogRef = this.dialog.open(MovePlannerModalComponent, {
       width: '800px',
       height: '600px',
-      data: { room, roomItems }
+      data: { room, roomItems, iconName, iconsList }
     });
   }
 }
