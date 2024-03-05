638b7c7a82dfeb07e05ffc830fe563eab8be280c
Josh Romisher <jromisher28@gmail.com>
2024-03-05 16:49:59 -0500

    added footer

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TuiSvgModule } from '@taiga-ui/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [TuiSvgModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
