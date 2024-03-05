ccb2b46d948b0aec2e2ad4969e51d403595d7ebf
Josh Romisher <jromisher28@gmail.com>
2024-03-05 12:56:13 -0500

    first shot at reviews

--- src/app/shared/services/google-maps-loader.service.ts
+++ src/app/shared/services/google-maps-loader.service.ts
@@ -22,18 +22,55 @@ export class GoogleMapsLoaderService {
     //Map
     this.map = new Map(
       document.getElementById("map") as HTMLElement, {
-      zoom: 7,
+      zoom: 14,
       center: position,
       mapId: "e953adf38f76874e"
     }
     );
-    console.log(this.map);
 
-    //Map marker on Choice Movers
-    const marker = new AdvancedMarkerElement({
-      map: this.map,
-      position: position,
-      title: 'Choice Movers Akron'
+    const request = {
+      placeId: "ChIJR0zbo4V49mIRynTpBCdPbC4",
+      fields: ["name", "reviews"]
+    };
+
+    const service = new google.maps.places.PlacesService(this.map);
+
+    const infowindow = new google.maps.InfoWindow();
+
+    service.getDetails(request, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        const marker = new AdvancedMarkerElement({
+          map: this.map,
+          position: position,
+          title: 'Choice Movers Akron'
+        });
+
+        google.maps.event.addListener(marker, "click", () => {
+          const content = document.createElement("div");
+
+          const nameElement = document.createElement("h2");
+
+          nameElement.textContent = place.name!;
+          content.appendChild(nameElement);
+
+          const placeIdElement = document.createElement("p");
+
+          placeIdElement.textContent = place.place_id!;
+          content.appendChild(placeIdElement);
+
+          const placeAddressElement = document.createElement("p");
+
+          placeAddressElement.textContent = place.formatted_address!;
+          content.appendChild(placeAddressElement);
+
+          infowindow.setContent(content);
+          infowindow.open(this.map, marker);
+        });
+      }
     });
   }
 }
