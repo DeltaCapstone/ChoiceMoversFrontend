7295bd6e25e8781b75aab7e5915acb5589e5096e
Josh Romisher <jromisher28@gmail.com>
2024-03-06 20:38:07 -0500

    initial job service

--- src/app/shared/services/jobs.service.ts
+++ src/app/shared/services/jobs.service.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { FeatureService } from './feature.service';
 import { Job } from '../../models/job.model';
+import { Room } from '../../models/room.model';
+import { User } from '../../models/user';
 
 /**
  * Service that provides an interface for creating and updating jobs from the customer facing Moving page.
@@ -13,6 +15,10 @@ import { Job } from '../../models/job.model';
 
 export class JobsService {
 
-  constructor(private http: HttpClient, private feature: FeatureService) { }
+  apiUrl: string = "";
+
+  constructor(private http: HttpClient, private feature: FeatureService) {
+    this.apiUrl = this.feature.getFeatureValue("api").url;
+  }
 
 }
