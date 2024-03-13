0cabdcbc819a770816ae95ed2a7ac8ffc79b126f
dominic <dominicf2001@gmail.com>
2024-03-13 15:06:57 -0400

    initial work for session integration

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Employee } from '../../models/user';
+import { Employee, LoginRequest } from '../../models/user';
 import { EmployeesService } from './employees.service';
-import { Observable, ReplaySubject, catchError, map, of, switchMap, tap } from 'rxjs';
-import { HttpRequest, HttpEvent, HttpHandler } from '@angular/common/http';
+import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FeatureService } from './feature.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
     user$: Observable<Employee | undefined> = of(undefined);
+    apiUrl: string;
     
-    constructor(private employeesService: EmployeesService) {
+    constructor(private employeesService: EmployeesService, private http: HttpClient, private feature: FeatureService) {
+        this.apiUrl = this.feature.getFeatureValue("api").url;
         // check if there is a stored token
         const token = sessionStorage.getItem("accessToken");
         const userName = sessionStorage.getItem("userName");
@@ -20,27 +23,62 @@ export class SessionService {
             this.user$ = this.employeesService.getProfile();
         }
     }
-    
+
+    setSessionValues(accessToken?: string, accessTokenExpiresAt?: string, refreshToken?: string, userName?: string){
+        accessToken = accessToken ?? sessionStorage.getItem("accessToken") ?? "";
+        accessTokenExpiresAt = accessTokenExpiresAt ?? sessionStorage.getItem("accessTokenExpiresAt") ?? "";
+        refreshToken = refreshToken ?? sessionStorage.getItem("refreshToken") ?? "";
+        userName = userName ?? sessionStorage.getItem("userName") ?? "";
+        
+        sessionStorage.setItem("accessToken", accessToken);
+        sessionStorage.setItem("userName", userName);
+        sessionStorage.setItem("accessTokenExpiresAt", accessTokenExpiresAt)
+        localStorage.setItem("refreshToken", refreshToken); 
+    }
+
+    isExpired(): boolean {
+        const expirationDateString = sessionStorage.getItem("accessTokenExpiresAt");
+        if (expirationDateString){
+            const expirationDate = new Date(expirationDateString);
+            return expirationDate < new Date();
+        }
+        else {
+            return true;
+        }
+    }
+
+    refresh(): Observable<boolean> {
+        const refreshToken = sessionStorage.getItem("refreshToken");
+        return this.http.post(`${this.apiUrl}/renewAccess`, {"refreshToken": refreshToken}).pipe(
+            tap((res: any) => this.setSessionValues(res["accessToken"], res["accessTokenExpiresAt"])),
+            map(_ => true),
+            catchError(err => {
+                console.error(err);
+                this.setSessionValues("", "", "", "");
+                return of(false);
+            })
+        )
+    }
+
     login(userName: string, passwordPlain: string): Observable<boolean> {
-        return this.employeesService.requestLogin(userName, passwordPlain).pipe(
+        const loginRequest: LoginRequest = {
+            userName: userName,
+            passwordPlain: passwordPlain
+        };
+        return this.http.post<LoginRequest>(`${this.apiUrl}/portal/login`, loginRequest).pipe(
             switchMap((res: any) => {
+                console.log(res);
                 const accessToken = res["accessToken"];
                 if (accessToken) {
-                    sessionStorage.setItem("accessToken", accessToken);
-                    sessionStorage.setItem("userName", userName);
-                    // If refreshToken is needed, uncomment the next line
-                    // localStorage.setItem("refreshToken", res["refreshToken"]); 
                     return this.employeesService.getProfile().pipe(
                         tap(profile => {
-                            console.log(profile);
                             this.user$ = of(profile);
+                            this.setSessionValues(accessToken, userName, res["accessTokenExpiresAt"], res["refreshToken"]);
                         }),
-                        map(() => true),
+                        map(_ => true),
                         catchError(err => {
                             console.error(err);
-                            sessionStorage.setItem("accessToken", "");
-                            // localStorage.setItem("refreshToken", "");
-                            sessionStorage.setItem("userName", ""); 
+                            this.setSessionValues("", "", "", "");
                             return of(false)
                         }) 
                     );
@@ -54,10 +92,9 @@ export class SessionService {
             catchError(() => of(false))
         );
     }
+
     logout() {
-        sessionStorage.setItem("accessToken", "");
-        // localStorage.setItem("refreshToken", "");
-        sessionStorage.setItem("userName", "");         
+        this.setSessionValues("", "", "", "");
         this.user$ = of(undefined);
     }
 
