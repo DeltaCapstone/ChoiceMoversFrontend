3179661b246bb6ecee2a7625a4028afda917f039
dominic <dominicf2001@gmail.com>
2024-03-14 16:40:53 -0400

    change name of models file

---
+++ src/app/models/employee.ts
@@ -0,0 +1,56 @@
+// -------------------
+// EMPLOYEE
+// -------------------
+export enum EmployeeType {
+    FullTime = "Full-time",
+    PartTime = "Part-time",
+    Manager = "Manager",
+    Admin= "Admin"
+}
+
+export interface IEmployee {
+    email: string;
+    firstName: string
+    lastName: string;
+    phonePrimary: string;
+    phoneOther: string[];
+    userName: string;
+    employeeType: EmployeeType;
+    employeePriority: number;
+}
+
+// should the password generated on the backend?
+export interface EmployeeCreateRequest extends IEmployee {
+    passwordPlain: string;
+}
+
+export interface LoginRequest {
+    userName: string;    
+    passwordPlain: string;    
+}
+
+export interface EmployeeProfileUpdateRequest {
+    email: string;
+    firstName: string
+    lastName: string;
+    phonePrimary: string;
+    phoneOther: string[];
+    userName: string;
+}
+
+export interface EmployeeTypePriorityRequest {
+    userName: string;
+    employeeType: EmployeeType;
+    employeePriority: number;
+}
+
+export class Employee implements IEmployee {
+    email: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    phonePrimary: string = "";
+    phoneOther: string[] = [];
+    userName: string = "";
+    employeeType: EmployeeType = EmployeeType.FullTime;
+    employeePriority: number = 0;
+}
