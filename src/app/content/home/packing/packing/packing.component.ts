34baf45367fd555c45a72c8d1a3cea32914aeb70
Thomas-Moore81 <tmoore81@kent.edu>
2024-04-03 16:03:58 -0400

    Majority of Frontend customer page css finished - Height is still an issue and not dynamic

--- src/app/content/home/packing/packing/packing.component.ts
+++ src/app/content/home/packing/packing/packing.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { TuiSvgModule } from '@taiga-ui/core';
-import { StartMoveButtonComponent } from '../../../../shared/components/start-move-button/start-move-button.component';
+import { MoveButtonComponent } from '../../../../shared/components/move-button/move-button.component';
 
 @Component({
   selector: 'app-packing',
   standalone: true,
-  imports: [TuiSvgModule, StartMoveButtonComponent],
+  imports: [TuiSvgModule, MoveButtonComponent],
   templateUrl: './packing.component.html',
   styleUrl: './packing.component.css'
 })
--- src/app/content/home/storage/storage/storage.component.ts
+++ src/app/content/home/storage/storage/storage.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { TuiSvgModule } from '@taiga-ui/core';
-import { StartMoveButtonComponent } from '../../../../shared/components/start-move-button/start-move-button.component';
+import { MoveButtonComponent } from '../../../../shared/components/move-button/move-button.component';
 
 @Component({
   selector: 'app-storage',
   standalone: true,
-  imports: [TuiSvgModule, StartMoveButtonComponent], 
+  imports: [TuiSvgModule, MoveButtonComponent], 
   templateUrl: './storage.component.html',
   styleUrl: './storage.component.css'
 })
